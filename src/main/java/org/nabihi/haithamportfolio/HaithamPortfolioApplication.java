3fd82ebf19ea8ee877c5668431967f30247fe9b4
kyrusho <jshn2004@hotmail.com>
2025-01-21 10:02:54 -0500

    project creation

---
+++ src/main/java/org/nabihi/haithamportfolio/HaithamPortfolioApplication.java
@@ -0,0 +1,13 @@
+package org.nabihi.haithamportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaithamPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaithamPortfolioApplication.class, args);
+	}
+
+}
